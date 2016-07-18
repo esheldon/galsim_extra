e5857c0cef7fbf6639211a105ec1c3d11f988b81
Erin Sheldon <erin.sheldon@gmail.com>
2016-07-18 11:54:45 -0400

    added first files

---
+++ setup.py
@@ -0,0 +1,13 @@
+import distutils
+from distutils.core import setup, Extension, Command
+import numpy
+
+setup(
+    name="galsim_extra", 
+    packages=['galsim_extra'],
+    version="0.1",
+)
+
+
+
+
