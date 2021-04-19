2fa95dff9e0efeae04eb1a96b227fb195ae49b7b
Matthew R. Becker <becker.mr@gmail.com>
2021-04-19 11:23:22 -0500

    ENH add license, use setuptools, add version string

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-import distutils
-from distutils.core import setup, Extension, Command
-import numpy
+import os
+from setuptools import setup, find_packages
+
+__version__ = None
+pth = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "galsim_extra",
+    "_version.py")
+with open(pth, 'r') as fp:
+    exec(fp.read())
 
 setup(
-    name="galsim_extra", 
-    packages=['galsim_extra'],
-    version="0.1",
+    name="galsim_extra",
+    packages=find_packages(),
+    version=__version__,
 )
-
-
-
-
